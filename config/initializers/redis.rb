134cb9d893867600cc2661bebaab14403c759970
Jacob Evelyn <jacobevelyn@gmail.com>
2015-08-02 23:54:00 -0400

    Add better connection pooling for Redis

--- config/initializers/redis.rb
+++
@@ -1,3 +0,0 @@
-$redis = ConnectionPool.new(size: 10) do
-  Redis.new(url: ENV["REDIS_URL"] || "redis://localhost:6379/0")
-end
--- config/puma.rb
+++ config/puma.rb
@@ -12,4 +12,9 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
+
+  redis_connections_per_process = Integer(ENV["REDIS_CONNS_PER_PROCESS"] || 5)
+  $redis = ConnectionPool.new(size: redis_connections_per_process) do
+    Redis.new(url: ENV["REDIS_URL"] || "redis://localhost:6379/0")
+  end
 end
