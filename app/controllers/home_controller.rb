b83e379b79d6d2f4c71320bc4592d6ac160e4cf2
Jacob Evelyn <jacobevelyn@gmail.com>
2015-09-19 15:27:09 -0400

    Experiment 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -77,7 +77,7 @@ class HomeController < ApplicationController
     `mkdir tmp/#{uuid}`
     `cp -r "public/Compute for Humanity.app" tmp/#{uuid}`
     `attr -q -s C4HReferralCode -V #{uuid} "tmp/#{uuid}/Compute for Humanity.app"`
-    `zip -q "tmp/#{uuid}/Compute for Humanity.zip" "tmp/#{uuid}/Compute for Humanity.app"`
+    `zip -q -r "tmp/#{uuid}/Compute for Humanity.zip" "tmp/#{uuid}/Compute for Humanity.app"`
     send_file "tmp/#{uuid}/Compute for Humanity.zip"
     `rm -rf tmp/#{uuid}`
     puts "Checkpoint!"
