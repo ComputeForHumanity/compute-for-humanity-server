595f402fb5048f498f077a426aa5f389cd2a8b9f
Jacob Evelyn <jacobevelyn@gmail.com>
2015-09-20 15:19:21 -0400

    Experiment 13

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -82,7 +82,7 @@ class HomeController < ApplicationController
     # `zip -q -r "tmp/#{uuid}/Compute for Humanity.zip" "tmp/#{uuid}/test.txt"`
     # `touch -t 200805101024 "tmp/#{uuid}/Compute for Humanity.app"`
     `zip -q -r "tmp/#{uuid}/Compute for Humanity.zip" "tmp/#{uuid}/Compute for Humanity.app"`
-    send_data File.read("tmp/#{uuid}/Compute for Humanity.zip"), filename: "Compute for Humanity.zip"
+    send_data File.read("public/Compute for Humanity.zip"), filename: "Compute for Humanity.zip"
     `rm -rf tmp/#{uuid}`
     puts "Checkpoint!"
   end
